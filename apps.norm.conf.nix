d7751107b84d4a9cf9d2768e29c83876af009820
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-09-28 18:58:34 +0700

    Create apps.norm.conf.nix

---
+++ apps.norm.conf.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+        #so u can run that shit (vcgencmd)
+        pkgs.libraspberrypi
+        pkgs.i2c-tools
+        pkgs.neovim
+        pkgs.deno
+        pkgs.zsh
+        pkgs.devenv
+        pkgs.flox
+  ];
+}
