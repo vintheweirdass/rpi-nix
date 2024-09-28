447cc93b04663b552a7c17d48b7ed2071c8497e5
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-09-28 18:59:43 +0700

    Update vnc.conf.nix

--- vnc.conf.nix
+++ vnc.conf.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 {
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22 80 443 5900 ];
+  networking.firewall.enable = false;
+  # TODO: make a separate firewall version and the non-firewall version | networking.firewall.allowedTCPPorts = [ 22 80 443 5900 ];
   nixpkgs.config.allowUnfree = true;
 }
