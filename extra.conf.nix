478b9c0a78a60edfbbf94cc0bbcd5fee1b36ab80
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-09-29 21:16:30 +0700

    Update extra.conf.nix

--- extra.conf.nix
+++ extra.conf.nix
@@ -3,15 +3,8 @@
   networking.firewall.enable = false;
   # TODO: make a separate firewall version and the non-firewall version | networking.firewall.allowedTCPPorts = [ 22 80 443 5900 ];
   nixpkgs.config.allowUnfree = true;
+  services.openssh.enable = true;
   #no zfs please
   boot.supportedFilesystems.zfs = lib.mkForce false;
   sdImage.compressImage = false;
-  nixpkgs.overlays = [
-      # Workaround: https://github.com/NixOS/nixpkgs/issues/154163
-      # modprobe: FATAL: Module sun4i-drm not found in directory
-      (final: super: {
-        makeModulesClosure = x:
-          super.makeModulesClosure (x // {allowMissing = true;});
-      })
-    ];
 }
