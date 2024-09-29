e3a3ff5f70455d36ef8203ff3e86007cb84cb1ff
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-09-29 19:44:02 +0700

    Update apps.remoteable.conf.nix

--- apps.remoteable.conf.nix
+++ apps.remoteable.conf.nix
@@ -3,7 +3,8 @@ let
   rev = "master"; # 'rev' could be a git rev, to pin the overlay.
   waylandOverlay = (import "${builtins.fetchTarball {
     url = "https://github.com/nix-community/nixpkgs-wayland/archive/${rev}.tar.gz"; 
-    sha256 = "";
+    #1. Set sha to "", then see the error, get the 'got' key, then done
+    sha256 = "09qlbz46fpkw3x3g03rp3sn9yk0mgghal6wlrmrmfqhqg700b8s8";
   }}/overlay.nix");
 in
   {
