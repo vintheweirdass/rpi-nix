c7b10e7f406591d6748d05a71f60631778adb5da
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-09-29 19:08:14 +0700

    Update apps.conf.nix

--- apps.conf.nix
+++ apps.conf.nix
@@ -2,7 +2,8 @@
 let
   rev = "master"; # 'rev' could be a git rev, to pin the overlay.
   url = "https://github.com/nix-community/nixpkgs-wayland/archive/${rev}.tar.gz";
-  waylandOverlay = (import "${builtins.fetchTarball url}/overlay.nix");
+  sha256 = ""
+  waylandOverlay = (import "${builtins.fetchTarball url, sha256}/overlay.nix");
 in
   {
     nixpkgs.overlays = [ waylandOverlay ];
