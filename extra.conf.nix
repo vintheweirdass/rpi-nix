c28d64acb10ced8e0e0504e55f3f868d3b6c83b6
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-09-30 08:06:10 +0700

    Update extra.conf.nix

--- extra.conf.nix
+++ extra.conf.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  hardware.enableRedistributableFirmware = false;
   networking.firewall.enable = false;
   # TODO: make a separate firewall version and the non-firewall version | networking.firewall.allowedTCPPorts = [ 22 80 443 5900 ];
   nixpkgs.config.allowUnfree = true;
