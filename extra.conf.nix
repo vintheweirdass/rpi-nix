e5992931b2de0cb77be3eedb659b3f7e1baeffe2
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-09-30 08:51:22 +0700

    Update extra.conf.nix

--- extra.conf.nix
+++ extra.conf.nix
@@ -3,7 +3,7 @@
   networking = {
     firewall.enable = false;
     networkManager.enable = true;
-    hostName = "rpinix"
+    hostName = "rpinix";
   };
   # TODO: make a separate firewall version and the non-firewall version | networking.firewall.allowedTCPPorts = [ 22 80 443 5900 ];
   nixpkgs.config.allowUnfree = true;
