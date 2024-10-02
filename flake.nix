02c010d1280b4f5ca17e8618bf2043959b3c4871
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-10-02 18:05:04 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,10 +24,12 @@
       #networking = {
         #networkmanager.enable = true;
       #};
-      services.xserver.enable = true;
-      services.xserver.displayManager.sddm.enable = true;
+      services = {
+        xserver.enable = true;
+        displayManager.sddm.enable = true;
       # TODO: delete that fucking 'xserver' on 24.05 and onwards
-      services.desktopManager.plasma6.enable = true;
+        desktopManager.plasma6.enable = true;
+      }
       # programs.sway.enable = true;
       #hardware = {
         # not for now
