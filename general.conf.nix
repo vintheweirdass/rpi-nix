d320b567c9cb7292d652dc141c4818c99851a5e1
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-10-03 10:58:41 +0700

    Update and rename extra.conf.nix to general.conf.nix

---
+++ general.conf.nix
@@ -0,0 +1,84 @@
+{ config, lib, pkgs, ... }:
+{
+  networking = {
+    firewall.enable = false;
+    #networkManager.enable = true;
+    #hostName = "rpinix";
+  };
+  # TODO: make a separate firewall version and the non-firewall version | networking.firewall.allowedTCPPorts = [ 22 80 443 5900 ];
+  nixpkgs.config.allowUnfree = true;
+  services.openssh.enable = true;
+  #no zfs please
+  boot.supportedFilesystems.zfs = lib.mkForce false;
+
+        # fuck uboot, thats the reason why you need to fork a fucking rpi linux fork, and its OPEN SOURCE FOR THAT SPECIFIC RPI DISTRO
+        # and now you, maintainers said "we dont want to repeat the problem from the previous rpi" (4b, 3b, and the earlier ones)
+        # and still defend to do a fucking plain linux
+        # u dumbass need to test it out, not just commenting out, then closing the issue without an hassle
+        # (no, im not talking about the stale bot, its litteraly the maintainer itself who closes so many raspberry pi issues on their official github
+        # and the issue still ongoing, not solved)
+
+        # and untill now im still suffering from this specific issue https://github.com/NixOS/nixpkgs/issues/173948
+        # and this shit https://github.com/NixOS/nixpkgs/issues/173948#issuecomment-1718069205 for the next update
+
+        # whos the raspberry pi nixos-specific maintainer at this point
+        #lib.mkForce false;
+        boot.loader.generic-extlinux-compatible.enable = true;
+        time.timeZone = "Asia/Jakarta";
+        system = {
+          disabledModules = [
+            "profiles/base.nix"
+          ];
+           system.stateVersion = "24.05"
+        };
+        fileSystems = {
+        "/" = {
+          fsType = "ext4";
+          device = "/dev/disk/by-uuid/${initial.evaluation.config.sdImage.rootPartitionUUID}";
+          # they dont have a built-in rtc, cuh
+          options = ["noatime"];
+        };
+        "/boot" = {
+          # nikocado avocado moment
+          fsType = "fat32";
+          device = "/dev/disk/by-label/${initial.evaluation.config.sdImage.firmwarePartitionName}";
+          options = [ "ro" ];
+          depends = [ "/" ];
+        }
+        };
+        security = {
+          sudo = {
+            enable = true;
+            wheelNeedsPassword = false;
+          };
+        };
+      hardware = {
+        # not for now
+        raspberry-pi."4".fkms-3d.enable = true;
+
+        # stupid, only works on compute model
+        # boot.kernelParams = [ "snd_bcm2835.enable_hdmi=1" ];
+
+        #if you switched to gnome, this prob works. not really tied to hardware tho
+        pulseaudio.enable = true;
+      };
+      services = {
+        xserver.enable = true;
+        displayManager.sddm.enable = true;
+        # TODO: delete that fucking 'xserver' on 24.05 and onwards
+        desktopManager.plasma6.enable = true;
+      };
+       users = {
+        #the nix.dev manual is outdated
+        #who the hell, who was using a fricking rpi 1??
+        #and the newer rpi4 dosent respect sysfs for gpio 
+        #users.groups.gpio = {};
+        users.cupglassdev = {
+              password = "admin";
+              shell = pkgs.zsh;
+              description = "change this, ok?";
+              isNormalUser = true;
+              extraGroups = ["wheel" "networkmanager"];
+        };
+       };
+}
