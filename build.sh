f4c0bc63230d5151d2ae57873217201160ca66f8
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-10-11 12:55:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+sudo nix flake update --extra-experimental-features nix-command --extra-experimental-features flakes     
 sudo nix build '.#nixosConfigurations.rpi-nix.config.system.build.sdImage' --extra-experimental-features nix-command --extra-experimental-features flakes
